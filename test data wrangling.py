2ff3ff779baaa8d39a7fb44871b56a9987275bd7
Data Scientist | Statistician <atefg1@hotmail.com>
2025-04-29 17:48:40 -0500

    c2

--- test data wrangling.py
+++ test data wrangling.py
@@ -1,8 +1,8 @@
 # Connection details
-server = 'your-server.database.windows.net'
-database = 'your-database'
-username = 'your-username'
-password = 'your-password'
+server = 'dmc2025.database.windows.net'
+database = 'Leads'
+username = 'atefgh'
+password = 'Waxxaw123'
 driver = '{ODBC Driver 17 for SQL Server}'
 
 conn_str = f"""
@@ -24,12 +24,11 @@ query = "SELECT * FROM your_existing_table"
 df = pd.read_sql(query, conn)
 
 # Do your transformation
-df['new_col'] = df['existing_col'] * 2  # Example transformation
+df=df[["Email Address","Address Line1","City","State","Zip"]]
 
 # Write to new table
-df.to_sql('your_new_table', con=conn, if_exists='replace', index=False, method='multi')  
+df.to_sql('Silver', con=conn, if_exists='replace', index=False, method='multi')  
 # OR use cursor.execute() for manual INSERTS if needed
 
-df=pd.read_excel(r"C:\Users\atefg\Downloads\SegmentDataPreview.xlsx")
-silver=df[["Email Address","Address Line1","City","State","Zip"]]
-silver
+
+
