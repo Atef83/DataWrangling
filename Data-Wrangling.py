c63eca1870bc132e8b0f0a918725a0e7d715b366
Data Scientist | Statistician <atefg1@hotmail.com>
2025-04-30 12:34:42 -0500

    gpt2

--- Data-Wrangling.py
+++ Data-Wrangling.py
@@ -1,7 +1,6 @@
 import jaydebeapi
 import pandas as pd
 
-# Connection string details
 server = 'dmc2025.database.windows.net'
 database = 'Leads'
 username = 'atefgh'
@@ -9,9 +8,8 @@ password = 'Waxxaw123'
 
 jdbc_url = f"jdbc:sqlserver://{server}:1433;databaseName={database};encrypt=true;trustServerCertificate=false"
 driver_class = "com.microsoft.sqlserver.jdbc.SQLServerDriver"
-jar_file = "sqljdbc.jar"
+jar_file = "jdbc_driver/sqljdbc_12.6/enu/mssql-jdbc-12.6.1.jre11.jar"  # Match extracted file
 
-# Connect using JDBC
 conn = jaydebeapi.connect(
     driver_class,
     jdbc_url,
@@ -19,12 +17,8 @@ conn = jaydebeapi.connect(
     jar_file
 )
 
-# Example: fetch data
 cursor = conn.cursor()
 cursor.execute("SELECT * FROM your_existing_table")
 rows = cursor.fetchall()
 columns = [desc[0] for desc in cursor.description]
 df = pd.DataFrame(rows, columns=columns)
-
-# Transform + Write (manual insert or via sqlalchemy alt)
-df = df[["Email Address", "Address Line1", "City", "State", "Zip"]]
