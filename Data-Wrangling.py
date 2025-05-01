3b7762d1090c991af77d612e4876fb62e869179a
Data Scientist | Statistician <atefg1@hotmail.com>
2025-05-01 14:35:08 -0500

    3

--- Data-Wrangling.py
+++ Data-Wrangling.py
@@ -1,34 +1,61 @@
+import jaydebeapi
+import pandas as pd
+
 # Connection details
 server = 'dmc2025.database.windows.net'
 database = 'Leads'
 username = 'atefgh'
 password = 'Waxxaw123'
-driver = '{JDBC Driver 17 for SQL Server}'
+driver = 'com.microsoft.sqlserver.jdbc.SQLServerDriver'
+jar_file = 'path_to_mssql_jdbc_auth-8.4.1.x86_64.jar'  # Download the JDBC driver
 
-conn_str = f"""
-DRIVER={driver};
-SERVER={server};
-DATABASE={database};
-UID={username};
-PWD={password};
-Encrypt=yes;
-TrustServerCertificate=no;
-Connection Timeout=30;
-"""
+conn_str = f"jdbc:sqlserver://{server}:1433;databaseName={database};user={username};password={password};encrypt=true;trustServerCertificate=false;"
 
 # Connect
-conn = JDBC.connect(conn_str)
+conn = jaydebeapi.connect(
+    driver,
+    conn_str,
+    jars=jar_file
+)
 
 # Run query
 query = "SELECT * FROM your_existing_table"
 df = pd.read_sql(query, conn)
 
 # Do your transformation
-df=df[["Email Address","Address Line1","City","State","Zip"]]
+df = df[["Email Address", "Address Line1", "City", "State", "Zip"]]
 
 # Write to new table
-df.to_sql('Silver', con=conn, if_exists='replace', index=False, method='multi')  
-# OR use cursor.execute() for manual INSERTS if needed
+# Since jaydebeapi doesn't play nicely with pandas to_sql,
+# you might need to use a different approach, like executemany or manual INSERTS
+cursor = conn.cursor()
+# Create table
+cursor.execute('''
+    IF OBJECT_ID('Silver', 'U') IS NOT NULL 
+    DROP TABLE Silver;
 
+    CREATE TABLE Silver (
+        "Email Address" VARCHAR(255),
+        "Address Line1" VARCHAR(255),
+        "City" VARCHAR(255),
+        "State" VARCHAR(255),
+        "Zip" VARCHAR(255)
+    );
+''')
 
+# Insert data
+insert_query = '''
+    INSERT INTO Silver ("Email Address", "Address Line1", "City", "State", "Zip")
+    VALUES (?, ?, ?, ?, ?)
+'''
 
+# Convert df to list of tuples
+data = list(df.itertuples(index=False, name=None))
+
+# Execute many
+cursor.executemany(insert_query, data)
+conn.commit()
+
+# Close the cursor and connection
+cursor.close()
+conn.close()
